5b5cdaca695c6facebe18cc68d3703af7877e6dc
Mr.Salahinia <salahimahdi938@gmail.com>
2023-01-12 19:06:06 +0330

    CreateClassAndAddProps

---
+++ DS.Graph/DS.Graph/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Graph
+{
+    public class Node<T>
+    {
+        public int Index { get; set; }
+        public T Data { get; set; }
+        public List<Node<T>> Neighbors { get; set; }
+        public List<int> Weights { get; set; }
+        public override string ToString()
+        {
+            return $"node with index: {Index}, Neighbors: {Neighbors.Count}";
+        }
+    }
+}
