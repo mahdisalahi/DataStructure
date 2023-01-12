323bb9866a8b2cdfd5183785d10dd41443e6eafa
Mr.Salahinia <salahimahdi938@gmail.com>
2023-01-12 19:31:54 +0330

    AddProps

---
+++ DS.Graph/DS.Graph/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Graph
+{
+    public class Graph<T>
+    {
+        public bool _isDirected = false;
+        public bool _isWeighted = false;
+
+        public Graph(bool isDirected, bool isWeighted)
+        {
+            _isDirected = isDirected;
+            _isWeighted = isWeighted;
+        }
+
+        public List<Node<T>> Nodes { get; set; }
+
+
+    }
+}
